acf5dc534130b163264d04e5110aa48a93824e45
PirateCoder13 <37461993+PirateCoder13@users.noreply.github.com>
2023-10-09 17:56:54 -0300

    Add files via upload

---
+++ PI2-main/blog/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="img/png" href="../assets/logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../navBar.css">
+    <title>Produtos do Futuro</title>
+</head>
+<style>
+    * {
+        margin: 0px;
+        padding: 0px
+    }
+
+    div.imgFooter {
+        background-image: url("../assets/aboutUsPlaceholder.png");
+        display: flex;
+        background-repeat: no-repeat;
+        background-size: contain;
+        width: 100%;
+        height: 1000px;
+        margin: 0px;
+    }
+
+    h2 {
+        text-align: center;
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 700;
+        margin: 0px;
+        color: #07A804;
+    }
+
+    p {
+        text-align: center;
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+        margin-top: 5px;
+        color: white;
+    }
+
+    div.box {
+        width: 50%;
+        background-color: white;
+        border-radius: 20px;
+        box-shadow: 3px 3px 3px 0px rgba(0, 0, 0, 1);
+        margin: 10px;
+        padding: 10px;
+        height: 220px;
+    }
+
+    div.box * {
+        color: black;
+        text-align: center;
+
+    }
+
+
+    div.center {
+        margin: 10px auto;
+    }
+
+    div.infoGrid {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
+
+<body>
+    <input class="menu-icon" type="checkbox" id="menu-icon" name="menu-icon" />
+    <label for="menu-icon"></label>
+    <nav class="nav">
+        <ul class="pt-5">
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="./index.php">Sobre nós</a></li>
+            <li><a href="">WIP</a></li>
+            <li><a href="">WIP</a></li>
+        </ul>
+    </nav>
+
+    <div class="imgFooter"> </div>
+    <!-- <h2>Equipe Produtos do Futuro</h2>
+    <p>Guiando Estudantes em Sua Jornada Profissional</p> -->
+
+
+
+    <div class="infoGrid">
+
+        <div class="box">
+            <h3>Introdução</h3>
+            <p>Somos o "Produtos do Futuro," uma plataforma inovadora nascida do projeto integrador da UniOpet em
+                Tecnologia em Análise e Desenvolvimento de Sistemas. Inicialmente criada pelo Grupo Um, nossa equipe se
+                inspirou na experiência prévia de nosso líder, que já havia desenvolvido um projeto semelhante. Ao longo
+                de nossa jornada, passamos por mudanças significativas.</p>
+        </div>
+
+        <div class="box">
+            <h3>Nossa Missão</h3>
+            <p>Entendemos as necessidades e aspirações dos estudantes universitários. Todos desejam adquirir experiência
+                em suas áreas de estudo e também ganhar um dinheiro extra. Ao mesmo tempo, aqueles que buscam serviços
+                desejam não apenas preços acessíveis, mas também a oportunidade de apoiar o desenvolvimento de futuros
+                profissionais.
+            </p>
+        </div>
+
+
+
+
+    </div>
+    <div class="infoGrid">
+
+        <div class="box">
+            <h3>Evolução do Grupo</h3>
+            <p>Inicialmente composto por Paulo Eduardo, Bryan Alexandre, José Augusto, João Trizotte, João Scarante e
+                Bruno Eufrasio, nossa equipe sofreu mudanças significativas. Bryan, Bruno e João Scarante não fazem mais
+                parte do grupo. No entanto, mantendo nosso compromisso com a missão, Antony Wippichy e João Gualberto se
+                juntaram ao projeto, trazendo novas perspectivas e habilidades valiosas.</p>
+        </div>
+
+        <div class="box">
+            <h3>Nosso Compromisso</h3>
+            <p>Estamos dedicados a criar um ambiente onde a juventude possa prosperar, onde a inovação e o talento
+                se unam para construir um futuro brilhante. Nosso site é mais do que uma plataforma de freelancers;
+                é um lugar onde sonhos se transformam em carreiras e onde cada passo é uma oportunidade de
+                aprendizado e crescimento.
+            </p>
+        </div>
+
+
+    </div>
+    <div class="box center">
+        <h3>Junte-se a Nós</h3>
+        <p>Convidamos você a fazer parte desta jornada conosco, seja como um estudante em busca de oportunidades ou
+            uma empresa em busca de jovens talentos. Juntos, podemos construir um futuro onde a paixão e o talento
+            se encontram, criando oportunidades sem limites.
+        </p>
+
+
+
+
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
