24f526e14fe896a1546e2c32d012e2147ff0500b
PewDizinho <55335712+PewDizinho@users.noreply.github.com>
2023-10-20 11:08:00 -0300

    Código de footer feito por Pirate
    Mostrando pro @PirateCoder13 como realizar uma PR do jeito certo,

--- sample.php
+++ sample.php
@@ -36,3 +36,131 @@
 </body>
 
 </html>
+
+
+<!-- footer -->
+<style>
+    footer {
+        background-color: black;
+    }
+
+    div.bgFooter {
+        padding: 2%;
+        width: 100%;
+        height: fit-content;
+        background-color: black;
+        color: white;
+        text-align: center;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+
+    div.infoFooter {
+        color: white;
+        font-family: Montserrat;
+        font-style: normal;
+        display: flex;
+        flex-direction: column;
+
+    }
+
+    div.infoFooter a {
+        color: white;
+        text-decoration: none;
+    }
+
+    div.logoFooter {
+        display: flex;
+        flex-direction: column;
+    }
+
+    div.social {
+        display: flex;
+        justify-content: space-around;
+
+    }
+
+    div.direitos {
+        height: fit-content;
+        background-color: #222222;
+        text-align: center;
+        padding: 0px 0px 1px 0px;
+    }
+</style>
+<footer>
+        <div class="bgFooter">
+            <div class="logoFooter">
+                <div>
+                    <img src="./assets/logoWhite.png" alt="Logo" width="60px" height="60px">
+                </div>
+                <p>Siga-nos</p>
+                <div class="social">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25"
+                        viewBox="0,0,256,256">
+                        <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                            stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                            font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                            style="mix-blend-mode: normal">
+                            <g transform="scale(5.12,5.12)">
+                                <path
+                                    d="M16,3c-7.17,0 -13,5.83 -13,13v18c0,7.17 5.83,13 13,13h18c7.17,0 13,-5.83 13,-13v-18c0,-7.17 -5.83,-13 -13,-13zM37,11c1.1,0 2,0.9 2,2c0,1.1 -0.9,2 -2,2c-1.1,0 -2,-0.9 -2,-2c0,-1.1 0.9,-2 2,-2zM25,14c6.07,0 11,4.93 11,11c0,6.07 -4.93,11 -11,11c-6.07,0 -11,-4.93 -11,-11c0,-6.07 4.93,-11 11,-11zM25,16c-4.96,0 -9,4.04 -9,9c0,4.96 4.04,9 9,9c4.96,0 9,-4.04 9,-9c0,-4.96 -4.04,-9 -9,-9z">
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25"
+                        viewBox="0,0,256,256">
+                        <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                            stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                            font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                            style="mix-blend-mode: normal">
+                            <g transform="scale(5.12,5.12)">
+                                <path
+                                    d="M25,3c-12.15,0 -22,9.85 -22,22c0,11.03 8.125,20.137 18.712,21.728v-15.897h-5.443v-5.783h5.443v-3.848c0,-6.371 3.104,-9.168 8.399,-9.168c2.536,0 3.877,0.188 4.512,0.274v5.048h-3.612c-2.248,0 -3.033,2.131 -3.033,4.533v3.161h6.588l-0.894,5.783h-5.694v15.944c10.738,-1.457 19.022,-10.638 19.022,-21.775c0,-12.15 -9.85,-22 -22,-22z">
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25"
+                        viewBox="0,0,256,256">
+                        <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                            stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                            font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                            style="mix-blend-mode: normal">
+                            <g transform="scale(5.12,5.12)">
+                                <path
+                                    d="M44.89844,14.5c-0.39844,-2.19922 -2.29687,-3.80078 -4.5,-4.30078c-3.29687,-0.69922 -9.39844,-1.19922 -16,-1.19922c-6.59766,0 -12.79687,0.5 -16.09766,1.19922c-2.19922,0.5 -4.10156,2 -4.5,4.30078c-0.40234,2.5 -0.80078,6 -0.80078,10.5c0,4.5 0.39844,8 0.89844,10.5c0.40234,2.19922 2.30078,3.80078 4.5,4.30078c3.5,0.69922 9.5,1.19922 16.10156,1.19922c6.60156,0 12.60156,-0.5 16.10156,-1.19922c2.19922,-0.5 4.09766,-2 4.5,-4.30078c0.39844,-2.5 0.89844,-6.10156 1,-10.5c-0.20312,-4.5 -0.70312,-8 -1.20312,-10.5zM19,32v-14l12.19922,7z">
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+            <div class="infoFooter">
+                <p>WEBSITE</p>
+                <a href="./index.php">Home</a>
+                <a href="../loginCadastro/cadastro.php">Login | Cadastro</a>
+                <a href="./blog/index.php">Blog</a>
+            </div>
+            <div class="infoFooter">
+                <p>INSTITUCIONAL</p>
+                <a href="">Quem somos</a>
+                <a href="">Equipe</a>
+                <a href="">Contato</a>
+            </div>
+            <div class="infoFooter">
+                <p>PRIVACIDADE & USO</p>
+                <a href="">Termos de Uso</a>
+                <a href="">Perguntas frequentes</a>
+                <a href="./">Política de privacidade</a>
+                <a href="./">Política de cookies</a>
+            </div>
+
+        </div>
+        <div class="direitos">
+            <p>© 2023 Projetos do Futuro. Todos os direitos reservados.</p>
+        </div>
+    </footer>
+    <!-- --------------------------------------------------------------------------------------- -->
